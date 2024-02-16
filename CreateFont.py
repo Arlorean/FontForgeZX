bfe473ac901383105c71da20431dcbc4e3dd7bee
Adam Davidson <adam@adam-davidson.com>
2024-02-16 21:40:45 +0000

    Generate 4x1 diacritics and base characters with double width alphanumerics (Japanese style)

--- CreateFont.py
+++ CreateFont.py
@@ -23,35 +23,88 @@ font.em = CHAR_SIZE
 font.ascent = 7*PIXEL_SIZE;
 font.descent = 1*PIXEL_SIZE;
 
-def create_pixel(pen, x, y):
-    pen.moveTo((x+0)*PIXEL_SIZE,(y+0)*PIXEL_SIZE)
-    pen.lineTo((x+0)*PIXEL_SIZE,(y+1)*PIXEL_SIZE)
-    pen.lineTo((x+1)*PIXEL_SIZE,(y+1)*PIXEL_SIZE)
-    pen.lineTo((x+1)*PIXEL_SIZE,(y+0)*PIXEL_SIZE)
+# Create mark and mkmk lookup tables
+scriptLanguages = (('DFLT',('dflt')),('latn',('dflt')))
+
+# NOTE: Must add 'mkmk' feature first, before 'mark' so it ends up at the bottom of the list(!)
+font.addLookup("mkmk-table", "gpos_mark2mark", None, (('mkmk',scriptLanguages),))
+font.addLookupSubtable("mkmk-table", "mkmk-subtable")
+font.addAnchorClass("mkmk-subtable", "mkmk")
+
+font.addLookup("mark-table", "gpos_mark2base", None, (('mark',scriptLanguages),))
+font.addLookupSubtable("mark-table", "mark-subtable")
+font.addAnchorClass("mark-subtable", "mark")
+
+
+def create_pixel(pen, x, y, xscale):
+    pen.moveTo((x+0)*PIXEL_SIZE*xscale,(y+0)*PIXEL_SIZE)
+    pen.lineTo((x+0)*PIXEL_SIZE*xscale,(y+1)*PIXEL_SIZE)
+    pen.lineTo((x+1)*PIXEL_SIZE*xscale,(y+1)*PIXEL_SIZE)
+    pen.lineTo((x+1)*PIXEL_SIZE*xscale,(y+0)*PIXEL_SIZE)
     pen.closePath()
 
+def create_byte(pen, x, y, byte, xscale):
+    while byte != 0:
+        if (byte & 1) == 1:
+            create_pixel(pen, x, y, xscale)
+        byte >>= 1
+        x -= 1
+
 # Define a simple square glyph
-def create_glyph(font, char, bytes):
+def create_glyph(font, char, bytes, width, xscale):
     glyph = font.createChar(char)
-    glyph.width = CHAR_SIZE
 
     pen = glyph.glyphPen()
 
-    for y in range(0,8):
-        byte = bytes[y]
-        x = 0
-        while byte != 0:
-            if (byte & 1) == 1:
-                create_pixel(pen, 7-x, 7-y-1)
-            byte >>= 1
-            x += 1
+    y = len(bytes)-2
+    for byte in bytes:
+        create_byte(pen, width-1, y, byte, xscale)
+        y -= 1
+
+    return glyph
 
 
-SPACE_CHAR = 32
+HALF_WIDTH = 32 # Half width forms of ASCII 
+FULL_WIDTH = 0xFF00 # Full width forms of ASCII 
 
 for i in range(0, NUM_CHARS):
     charBytes = bytes[i*8:(i+1)*8]
-    create_glyph(font, i+SPACE_CHAR, charBytes)
+
+    glyph = create_glyph(font, i+HALF_WIDTH, charBytes, 8, 0.5)
+    glyph.width = CHAR_SIZE//2
+
+    glyph = create_glyph(font, i+FULL_WIDTH, charBytes, 8, 1)
+    glyph.width = CHAR_SIZE
+
+
+DIACRITIC_MARKS = 0x0300 # Combining Diacritical Marks
+
+for i in range(0, 16):
+    charBytes = [i]
+    glyph = create_glyph(font, i+DIACRITIC_MARKS, charBytes, 4, 1)
+    glyph.glyphname = "mark"+str(i)
+    glyph.width = 0 #CHAR_SIZE//2
+    
+    glyph.addAnchorPoint("mark", "mark", 0, -256)
+
+    glyph.addAnchorPoint("mkmk", "basemark", 0, 0)
+    glyph.addAnchorPoint("mkmk", "mark", 0, -256)
+
+
+PUA = 0xE000 # Private Use Area
+
+for i in range(0, 16):
+    charBytes = [i]
+    glyph = create_glyph(font, i+PUA, charBytes, 4, 1)
+    glyph.glyphname = "base"+str(i)
+    glyph.width = CHAR_SIZE//2
+
+    glyph.addAnchorPoint("mark", "base", 0, 0)
+
+
+# Save font file
+sfdFileName = os.path.splitext(filename)[0]+'.sfd'
+font.save(sfdFileName)
 
 # Generate the TrueType font file
 ttfFileName = os.path.splitext(filename)[0]+'.ttf'
